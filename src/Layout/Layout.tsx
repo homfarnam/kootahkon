11e9c945380e2c3081530400dd37c44487c6383d
homfarnam <homfarnam@gmail.com>
2021-04-09 23:55:17 +0430

    chore: Delete head

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -3,9 +3,6 @@ interface LayoutProps {}
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <head>
-        <title>Kootahkon</title>
-      </head>
       <div className="flex flex-col justify-between h-full min-h-screen w-full text-white bg-gradient-to-br from-gray-600 via-teal-700 to-gray-800">
         {children}
       </div>
