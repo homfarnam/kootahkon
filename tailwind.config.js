a1337fea27b11291985461ca9002cef55acc533d
homfarnam <homfarnam@gmail.com>
2021-04-14 22:47:00 +0430

    Add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ["Vazir"],
+      },
+    },
   },
   variants: {},
   plugins: [],
   purge: {
     // Filenames to scan for classes
     content: [
-      './src/**/*.html',
-      './src/**/*.js',
-      './src/**/*.jsx',
-      './src/**/*.ts',
-      './src/**/*.tsx',
-      './public/index.html',
+      "./src/**/*.html",
+      "./src/**/*.js",
+      "./src/**/*.jsx",
+      "./src/**/*.ts",
+      "./src/**/*.tsx",
+      "./public/index.html",
     ],
     // Options passed to PurgeCSS
     options: {
@@ -20,4 +24,4 @@ module.exports = {
       // safelist: [],
     },
   },
-}
+};
