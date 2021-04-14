75811630df5cb353c4d390e8d7fe256f0f2195ed
homfarnam <homfarnam@gmail.com>
2021-04-14 22:46:40 +0430

    Add header

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -3,7 +3,16 @@ interface LayoutProps {}
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <div className="flex flex-col justify-between h-full min-h-screen w-full text-white bg-gradient-to-br from-gray-600 via-teal-700 to-gray-800">
+      <div className="flex flex-col justify-between h-full min-h-screen w-full text-black  ">
+        <header className="py-5 bg-gradient-to-t from-blue-200 to-blue-300">
+          <div className="flex w-full justify-center">
+            <ul className="flex m-auto space-x-4 font-semibold">
+              <li>تماس با کوتاه کن</li>
+              <li>درباره کوتاه کن</li>
+              <li>خانه</li>
+            </ul>
+          </div>
+        </header>
         {children}
       </div>
     </>
