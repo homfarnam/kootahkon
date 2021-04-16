a58d9f011b21cf3dc541bf697dd9558e64bcd514
homfarnam <homfarnam@gmail.com>
2021-04-16 12:11:08 +0430

    Create contact us page

---
+++ src/Pages/Contact.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "../Layout/Layout";
+
+interface ContactProps {}
+
+const Contact: React.FC<ContactProps> = () => {
+  return (
+    <Layout>
+      <div>contact</div>
+    </Layout>
+  );
+};
+
+export default Contact;
