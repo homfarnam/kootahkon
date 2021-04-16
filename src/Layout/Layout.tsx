ff411c60011731b37fd202db86c9da748c60933a
homfarnam <homfarnam@gmail.com>
2021-04-16 12:22:00 +0430

    Change nav style

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -7,19 +7,19 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <>
       <div className="flex flex-col justify-between h-full min-h-screen w-full text-white  ">
         <header className="py-5 bg-[#000080]">
-          <div className="flex w-full justify-between">
+          <div className="flex w-full items-center justify-between">
             <div>
               <ul className="flex space-x-3 ml-5">
-                <Link to="/contact-us">
+                <Link to="/contact-us" className=" hover:text-yellow-400">
                   <li>تماس با ما</li>
                 </Link>
-                <Link to="/about">
+                <Link to="/about" className=" hover:text-yellow-400">
                   <li>درباره ما</li>
                 </Link>
               </ul>
             </div>
             <div className="flex mx-5 space-x-4 font-semibold">
-              <Link to="/">
+              <Link to="/" className="text-2xl">
                 <a href="/">کوتاه کن</a>
               </Link>
             </div>
