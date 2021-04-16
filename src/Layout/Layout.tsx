d71b887b466a71930c1b417c6651e6c5b9c495be
homfarnam <homfarnam@gmail.com>
2021-04-16 12:10:49 +0430

    Change nav bar

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,16 +1,28 @@
+import { Link } from "react-router-dom";
+
 interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
-      <div className="flex flex-col justify-between h-full min-h-screen w-full text-black  ">
-        <header className="py-5 bg-gradient-to-t from-blue-200 to-blue-300">
-          <div className="flex w-full justify-center">
-            <ul className="flex m-auto space-x-4 font-semibold">
-              <li>تماس با کوتاه کن</li>
-              <li>درباره کوتاه کن</li>
-              <li>خانه</li>
-            </ul>
+      <div className="flex flex-col justify-between h-full min-h-screen w-full text-white  ">
+        <header className="py-5 bg-[#000080]">
+          <div className="flex w-full justify-between">
+            <div>
+              <ul className="flex space-x-3 ml-5">
+                <Link to="/contact-us">
+                  <li>تماس با ما</li>
+                </Link>
+                <Link to="/about">
+                  <li>درباره ما</li>
+                </Link>
+              </ul>
+            </div>
+            <div className="flex mx-5 space-x-4 font-semibold">
+              <Link to="/">
+                <a href="/">کوتاه کن</a>
+              </Link>
+            </div>
           </div>
         </header>
         {children}
