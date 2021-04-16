189d025274b12a5053b0c6c01077311a855dbf0e
homfarnam <homfarnam@gmail.com>
2021-04-16 12:31:49 +0430

    Add texts

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -6,7 +6,19 @@ interface AboutProps {}
 const About: React.FC<AboutProps> = () => {
   return (
     <Layout>
-      <div>about</div>
+      <div className="w-full h-full flex justify-center text-black">
+        <div className="mt-5 w-1/2 flex flex-col items-center justify-start">
+          <h3 className="font-semibold text-3xl">درباره ما</h3>
+          <div className="w-1/2 mt-5">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+              corporis deserunt, magni maxime voluptate ducimus iste minima
+              dolor similique dignissimos, pariatur tenetur? Placeat, iste eius
+              ex reiciendis laboriosam mollitia aliquid!
+            </p>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -6,7 +6,19 @@ interface ContactProps {}
 const Contact: React.FC<ContactProps> = () => {
   return (
     <Layout>
-      <div>contact</div>
+      <div className="w-full h-full flex justify-center text-black">
+        <div className="mt-5 w-1/2 flex flex-col items-center justify-start">
+          <h3 className="font-semibold text-3xl">تماس ما</h3>
+          <div className="w-1/2 mt-5">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+              corporis deserunt, magni maxime voluptate ducimus iste minima
+              dolor similique dignissimos, pariatur tenetur? Placeat, iste eius
+              ex reiciendis laboriosam mollitia aliquid!
+            </p>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
