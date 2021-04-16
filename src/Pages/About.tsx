9321c8d08f1493e816e95d17b68bc178d5829359
homfarnam <homfarnam@gmail.com>
2021-04-16 14:25:28 +0430

    Change styles

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -9,12 +9,16 @@ const About: React.FC<AboutProps> = () => {
       <div className="w-full h-full flex justify-center text-black">
         <div className="mt-5 w-1/2 flex flex-col items-center justify-start">
           <h3 className="font-semibold text-3xl">درباره ما</h3>
-          <div className="w-1/2 mt-5">
-            <p>
-              کوتاه‌کن، یک سرویس کوتاه کننده پیوند است که به عنوان یک پروژه آخر هفته‌ای ساخته شد. کوتاه‌کن به شما کمک می‌کند تا پیوندهای بلند خود را در کوتاه‌ترین زمان به پیوندهای کوتاه و قابل اشاره در جاهای دیگر تبدیل کنید<div className=""></div>
+          <div className="w-1/2 mt-5 text-right">
+            <p style={{ direction: "rtl" }}>
+              کوتاه‌کن، یک سرویس کوتاه کننده پیوند است که به عنوان یک پروژه آخر
+              هفته‌ای ساخته شد. کوتاه‌کن به شما کمک می‌کند تا پیوندهای بلند خود
+              را در کوتاه‌ترین زمان به پیوندهای کوتاه و قابل اشاره در جاهای دیگر
+              تبدیل کنید.
             </p>
-            <p>
-              کوتاه‌کن در مدت زمانی کوتاه و با کمک تکنولوژی‌های Ruby, Sinatra, SQLite و React نوشته شده است. 
+            <p style={{ direction: "rtl" }}>
+              کوتاه‌کن در مدت زمانی کوتاه و با کمک تکنولوژی‌های Ruby, Sinatra,
+              SQLite و React نوشته شده است.
             </p>
           </div>
         </div>
--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -9,21 +9,17 @@ const Contact: React.FC<ContactProps> = () => {
       <div className="w-full h-full flex justify-center text-black">
         <div className="mt-5 w-1/2 flex flex-col items-center justify-start">
           <h3 className="font-semibold text-3xl">تماس با ما</h3>
-          <div className="w-1/2 mt-5">
-            <h4 className="font-semibold text-2xl">محمدرضا حقیری</h4>
-            <p>
-              طراحی و پیاده‌سازی بکند، عملیات دواپس
-            </p>
-            <p>
-              haghiri75@gmail.com
-            </p>
-            <h4 className="font-semibold text-2xl">فرنام همایونفر</h4>
-            <p>
-               طراحی رابط کاربری و پیاده‌سازی فرانتند
-            </p>
-            <p>
-            homfarnam@gmail.com
-            </p>
+          <div className="w-1/2 mt-5 flex flex-col justify-center items-center space-y-5">
+            <div className="flex flex-col items-center">
+              <h4 className="font-semibold text-2xl">محمدرضا حقیری</h4>
+              <p>طراحی و پیاده‌سازی ،Backend عملیات دواپس</p>
+              <p>haghiri75@gmail.com</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <h4 className="font-semibold text-2xl">فرنام همایونفر</h4>
+              <p> Frontend طراحی رابط کاربری و پیاده‌سازی </p>
+              <p>homfarnam@gmail.com</p>
+            </div>
           </div>
         </div>
       </div>
