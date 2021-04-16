03bac98bf23bd6c5b25d968b11400e823ab52f3c
homfarnam <homfarnam@gmail.com>
2021-04-16 12:11:01 +0430

    Create about us page

---
+++ src/Pages/About.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "../Layout/Layout";
+
+interface AboutProps {}
+
+const About: React.FC<AboutProps> = () => {
+  return (
+    <Layout>
+      <div>about</div>
+    </Layout>
+  );
+};
+
+export default About;
